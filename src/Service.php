cecdd4904c23f8a3f6474026e25fa71bc12e93e6
Ryan Mahoney <ryan@virtuecenter.com>
2015-01-11 15:37:51 -0500

    better object calisthenics

--- src/Service.php
+++ src/Service.php
@@ -24,195 +24,95 @@
  */
 namespace Opine\Build;
 
-use MongoClient;
-use MongoDB;
-use MongoDate;
 use RecursiveIteratorIterator;
 use RecursiveDirectoryIterator;
 use FilesystemIterator;
 use Exception;
-use Memcache;
-use Pheanstalk_Pheanstalk;
 use Opine\Interfaces\Cache as CacheInterface;
-use Opine\Interfaces\Route as RouteInterface;
 
 class Service
 {
-    private $root = false;
-    private $pubSubModel;
-    private $collectionModel;
-    private $helperServiceModel;
-    private $configModel;
-    private $formModel;
+    private $root;
     private $cache;
-    private $bundleModel;
-    private $bundleBuild;
-    private $search;
-    private $route;
-    private $routeModel;
-    private $containerCache;
-    private $handlebarService;
-    private $languageModel;
+    private $cacheKey;
     private $config;
-    private $db;
+    private $bundles;
+    private $container;
+    private $route;
+    private $collections;
+    private $forms;
+    private $helpers;
+    private $templates;
+    private $topics;
+    private $person;
+    private $languages;
 
-    public function __construct(
-        $root,
-        $pubSubModel,
-        $collectionModel,
-        $helperServiceModel,
-        $configModel,
-        $formModel,
-        $bundleModel,
-        $bundleBuild,
-        CacheInterface $cache,
-        $search,
-        RouteInterface $route,
-        $routeModel,
-        $containerCache,
-        $handlebarService,
-        $languageModel,
-        $config,
-        $db)
+    public function __construct($root, CacheInterface $cache, $config, $bundles, $container, $route, $collections, $forms, $helpers, $templates, $topics, $person, $languages)
     {
         $this->root = $root;
-        $this->pubSubModel = $pubSubModel;
-        $this->collectionModel = $collectionModel;
-        $this->helperServiceModel = $helperServiceModel;
-        $this->configModel = $configModel;
-        $this->formModel = $formModel;
-        $this->bundleModel = $bundleModel;
-        $this->bundleBuild = $bundleBuild;
         $this->cache = $cache;
-        $this->search = $search;
-        $this->route = $route;
-        $this->routeModel = $routeModel;
-        $this->containerCache = $containerCache;
-        $this->handlebarService = $handlebarService;
-        $this->languageModel = $languageModel;
+        $this->cacheKey = $this->root.'-opine';
         $this->config = $config;
-        $this->db = $db;
+        $this->bundles = $bundles;
+        $this->container = $container;
+        $this->route = $route;
+        $this->collections = $collections;
+        $this->forms = $forms;
+        $this->helpers = $helpers;
+        $this->templates = $templates;
+        $this->topics = $topics;
+        $this->person = $person;
+        $this->languages = $languages;
     }
 
     public function project()
     {
-        try {
-            $this->search->indexCreateDefault();
-        } catch (Exception $e) {
-            //echo 'Search Index Error: ', $e->getMessage(), "\n";
-        }
+        //clear memcache and filesystem
         $this->clearCache();
         $this->clearFileCache();
-        $this->salt();
-        $this->config();
-        $this->directories();
-        $this->db();
-        $this->bundles();
-        $this->container();
-        $this->route();
-        $this->collections();
-        $this->forms();
-        $this->helpers();
-        $this->templatesCompile();
-        $this->topics();
-        $this->languages();
-        try {
-            $this->adminUserFirst();
-        } catch (Exception $e) {
-        }
-        echo 'Built', "\n";
+
+        //build each type of thing
+        $this->config->build();
+        $this->bundles->build();
+        $this->container->build();
+        $this->bundles->build();
+        $this->container->build();
+        $this->route->build();
+        $this->collections->build();
+        $this->forms->build();
+        $this->helpers->build();
+        $this->templates->build();
+        $this->topics->build();
+        $this->person->build();
+        $this->languages->build();
+
+        //put cache data into memcache
+        $cache = [
+            'collections' => $this->getFile($this->root.'/../var/cache/collections.json'),
+            'forms'       => $this->getFile($this->root.'/../var/cache/forms.json'),
+            'bundles'     => $this->getFile($this->root.'/../var/cache/bundles.json'),
+            'topics'      => $this->getFile($this->root.'/../var/cache/topics.json'),
+            'routes'      => $this->getFile($this->root.'/../var/cache/routes.json'),
+            'container'   => $this->getFile($this->root.'/../var/cache/container.json'),
+            'languages'   => $this->getFile($this->root.'/../var/cache/languages.json'),
+            'config'      => $this->getFile($this->root.'/../var/cache/config.json')
+        ];
+
+        $this->cache->set($this->cacheKey, json_encode($cache));
+        return $cache;
     }
 
-    public function languages()
+    private function getFile ($path)
     {
-        $this->cache->set($this->root.'-languages', $this->languageModel->build(), 0);
-    }
-
-    public function templatesCompile()
-    {
-        $this->handlebarService->build();
-    }
-
-    public function environmentCheck()
-    {
-        $authConfigFile = $this->root.'/../config/auth.php';
-        if (file_exists($authConfigFile)) {
-            echo 'Good: Authentication salt file already exists.', "\n";
+        if (!file_exists($path)) {
+            return [];
         }
-
-        //mongo
-        if (class_exists('\MongoClient', false)) {
-            echo 'Good: MongoDB client driver is installed.', "\n";
-        } else {
-            echo 'Problem: MongoDB client driver not installed.', "\n";
-        }
-        $dbConfig = $this->config->get('db');
-        if (is_array($dbConfig) && count($dbConfig) > 0) {
-            echo 'Good: Database config file exists.', "\n";
-            try {
-                $client = new MongoClient($dbConfig['conn']);
-                $collections = $client->{$dbConfig['name']}->getCollectionNames();
-                echo 'Good: can connect to database.', "\n";
-            } catch (Exception $e) {
-                echo 'Problem: can not connect to database: ', $e->getMessage(), "\n";
-            }
-        } else {
-            echo 'Problem: Database config file does not exists.', "\n";
-        }
-
-        //memcache
-        if (class_exists('\Memcache', false)) {
-            echo 'Good: Memcache client driver is installed.', "\n";
-            $memcache = new Memcache();
-            try {
-                $result = @$memcache->pconnect('localhost', 11211);
-                if ($result !== false) {
-                    echo 'Good: Memcache connection made.', "\n";
-                } else {
-                    echo 'Problem: Memcache connection failed.', "\n";
-                }
-            } catch (Exception $e) {
-                echo 'Problem: Memcache: ', $e->getMessage(), "\n";
-            }
-        } else {
-            echo 'Problem: Memcache client driver is not installed.', "\n";
-        }
-
-        //beanstalkd
-        if (class_exists('\Pheanstalk_Pheanstalk')) {
-            echo 'Good: Queue client driver is installed.', "\n";
-            $queue = new Pheanstalk_Pheanstalk('127.0.0.1');
-            try {
-                if ($queue->getConnection()->isServiceListening() != true) {
-                    echo 'Problem: Queue connetion not made.', "\n";
-                } else {
-                    echo 'Good: Queue connetion made.', "\n";
-                }
-            } catch (\Exception $e) {
-                echo 'Problem: Queue: ', $e->getMessage(), "\n";
-            }
-        } else {
-            echo 'Problem: Pheanstalkd client driver is not installed.', "\n";
-        }
-    }
-
-    private function config()
-    {
-        $this->configModel->build($this->root);
+        return json_decode(file_get_contents($path));
     }
 
     private function clearCache()
     {
-        $this->cache->deleteBatch([
-            $this->root.'-collections',
-            $this->root.'-forms',
-            $this->root.'-bundles',
-            $this->root.'-topics',
-            $this->root.'-routes',
-            $this->root.'-container',
-            $this->root.'-languages',
-            $this->root.'-config',
-        ]);
+        $this->cache->delete($this->cacheKey);
     }
 
     private function clearFileCache()
@@ -222,157 +122,5 @@ class Service
             $path->isDir() && !$path->isLink() ? rmdir($path->getPathname()) : unlink($path->getPathname());
         }
         rmdir($dirPath);
-    }
-
-    private function salt()
-    {
-        $authConfigFile = $this->root.'/../config/settings/auth.yml';
-        if (file_exists($authConfigFile)) {
-            return;
-        }
-        file_put_contents($authConfigFile, 'settings:
-    salt: '.uniqid().uniqid().uniqid());
-    }
-
-    private function adminUserFirst()
-    {
-        if (!class_exists('MongoClient', false)) {
-            echo 'Note: MongoDB client driver not installed.', "\n";
-
-            return;
-        }
-        try {
-            $auth = require $this->root.'/../config/auth.php';
-            if (!isset($auth['salt'])) {
-                echo 'Problem: No Salt set in auth config file';
-            }
-            $found = $this->db->collection('users')->findOne(['groups' => 'manager'], ['_id', 'groups']);
-            if (isset($found['_id'])) {
-                echo 'Good: Superadmin already exists.', "\n";
-
-                return;
-            }
-            $id = $this->db->id();
-            $dbURI = 'users:'.(string) $id;
-            $this->db->document($dbURI)->upsert([
-                '_id'          => $id,
-                'first_name'   => 'Admin',
-                'last_name'    => 'Admin',
-                'email'        => 'admin@website.com',
-                'groups'       => ['manager'],
-                'password'     => sha1($auth['salt'].'password'),
-                'created_date' => new MongoDate(strtotime('now')),
-                'dbURI'        => 'users:'.(string) $id,
-                'acl'          => ['manager'],
-            ]);
-            echo 'Good: Superuser created. admin@website.com : password', "\n";
-        } catch (Exception $e) {
-            echo 'Note: Can not create manager superuser because database credentials not yet set, or:', $e->getMessage(), "\n";
-        }
-    }
-
-    private function bundles()
-    {
-        $this->cache->set($this->root.'-bundles', $this->bundleModel->build(), 0);
-        $this->bundleBuild->build();
-    }
-
-    private function collections()
-    {
-        $this->cache->set($this->root.'-collections', $this->collectionModel->build());
-    }
-
-    private function forms()
-    {
-        $this->cache->set($this->root.'-forms', $this->formModel->build());
-    }
-
-    private function helpers()
-    {
-        $this->helperServiceModel->buildAll();
-        $this->handlebarService->helpersLoad();
-    }
-
-    private function topics()
-    {
-        $this->cache->set($this->root.'-topics', json_encode($this->pubSubModel->build()));
-    }
-
-    private function db()
-    {
-        $dbPath = $this->root.'/../config/db.php';
-        if (!file_exists($dbPath)) {
-            file_put_contents($dbPath, file_get_contents(__DIR__.'/../static/db.php'));
-        }
-    }
-
-    private function route()
-    {
-        $this->routeModel->build();
-        $routes = json_encode($this->route->cacheGenerate());
-        $this->cache->set($this->root.'-routes', $routes);
-    }
-
-    private function directories()
-    {
-        foreach (['css', 'js', 'layouts', 'partials', 'images', 'fonts', 'helpers'] as $dir) {
-            $dirPath = $this->root.'/'.$dir;
-            if (!file_exists($dirPath)) {
-                mkdir($dirPath);
-            }
-        }
-        foreach (['config', 'config/routes', 'config/settings', 'config/containers', 'config/collections', 'config/forms', 'config/managers', 'config/layouts', 'app', 'app/models', 'app/views', 'app/controllers', 'app/helpers', 'var', 'var/cache', 'var/log'] as $dir) {
-            $dirPath = $this->root.'/../'.$dir;
-            if (!file_exists($dirPath)) {
-                mkdir($dirPath);
-            }
-        }
-        foreach (['collections', 'documents', 'forms'] as $dir) {
-            $dirPath = $this->root.'/layouts/'.$dir;
-            if (!file_exists($dirPath)) {
-                mkdir($dirPath);
-            }
-            $dirPath = $this->root.'/partials/'.$dir;
-            if (!file_exists($dirPath)) {
-                mkdir($dirPath);
-            }
-            $dirPath = $this->root.'/../config/layouts/'.$dir;
-            if (!file_exists($dirPath)) {
-                mkdir($dirPath);
-            }
-        }
-    }
-
-    public function container()
-    {
-        $this->containerCache->clear();
-        $this->containerCache->read($this->root.'/../config/containers/container.yml');
-        $this->cache->set($this->root.'-container', $this->containerCache->write());
-    }
-
-    public function collectionInstall($collection)
-    {
-        $path = $this->root.'/../collections/'.$collection.'.php';
-        if (file_exists($path)) {
-            echo $path, ': already exists.', "\n";
-
-            return;
-        }
-        $remote = 'https://raw.githubusercontent.com/Opine-Org/Collection/master/available/'.$collection.'.php';
-        file_put_contents($path, file_get_contents($remote));
-        echo $path, ': saved.', "\n";
-    }
-
-    public function managerInstall($manager)
-    {
-        $path = $this->root.'/../managers/'.$manager.'.php';
-        if (file_exists($path)) {
-            echo $path, ': already exists.', "\n";
-
-            return;
-        }
-        $remote = 'https://raw.githubusercontent.com/Opine-Org/Semantic-CM/master/available/'.$manager.'.php';
-        file_put_contents($path, file_get_contents($remote));
-        echo $path, ': saved.', "\n";
     }
 }
